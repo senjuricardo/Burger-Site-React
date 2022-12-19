f5282b61f979e62b9fc288cd8fa2fcbf167863ca
Ricardo <ricardo.sousa13@hotmail.com>
2022-12-19 23:07:18 +0000

    added remove sideDrawer on click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Auxs'
 import  './Layout.css'
 import ToolBar from '../Navigation/ToolBar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <ToolBar></ToolBar>
-        <SideDrawer></SideDrawer>
-        <main className='Content'>{props.children}</main>
-    </Aux>
-)
 
-export default layout
+class Layout extends Component {
+    state={
+            showSideDrawer: true
+    }
+    SideDrawerCLoseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    render() {
+        return(
+            <Aux>
+            <ToolBar></ToolBar>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerCLoseHandler}></SideDrawer>
+            <main className='Content'>{this.props.children}</main>
+        </Aux>
+        )
+    }
+}
+  
+export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,14 +2,22 @@ import React from "react"
 import Logo from "../../Logo/Logo"
 import NavigationItems from "../NavigationItems/NavigationItems"
 import './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxs'
 
-const sideDrawer = (props)=> {
-
-    return(
-        <div className="SideDrawer">
-            <Logo height="11%"/>
-             <NavigationItems></NavigationItems> 
-        </div>
+const sideDrawer = (props) => {
+    let attachedClasses = ["SideDrawer", "Close"];
+    if(props.open){
+      attachedClasses=  ["SideDrawer", "Open"];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%" />
+                <NavigationItems></NavigationItems>
+            </div>
+        </Aux>
     )
 }
 
