cab8bc32e5b9edc035ffadb991ffa1f023471f27
Ricardo <ricardo.sousa13@hotmail.com>
2023-01-09 22:14:42 +0000

    added route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div >
-      <Layout>
-        <BurgerBuilder/>
-        <Checkout></Checkout>
-      </Layout>
-      
-    </div>
+    <div>
+        <Layout>
+          <Routes>
+            {/* <Route path="/checkout" component={Checkout}  />
+            <Route path="/" exact component={BurgerBuilder} /> */}
+            <Route path="/checkout"  element={<Checkout/>}></Route>
+            <Route path="/" exact element={<BurgerBuilder/>}></Route>
+         </Routes>
+        </Layout>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const app = (
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>
 );
 
